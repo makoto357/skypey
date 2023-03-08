dac2f2e074c9e90b53a95209888d37479ae10d32
Claire Chu(朱晏禛) <claire_chu@htc.com>
2023-03-08 10:49:36 +0800

    add user header

--- src/components/ChatWindow/index.js
+++ src/components/ChatWindow/index.js
@@ -1,6 +1,34 @@
 import './ChatWindow.css';
+import store from '../../store';
+
+const Header = ({user}) => {
+  const {name, status} = user;
+  return (
+    <header className="Header">
+      <h1 className="Header__name">{name}</h1>
+      <p className="Header__status">{status}</p>
+    </header>
+  );
+};
+
+const Chats = () => {
+  return <div>chats</div>;
+};
+const MessageInput = () => {
+  return <div>MessageInput</div>;
+};
+
 function ChatWindow({activeUserId}) {
-  return <div className="ChatWindow">ChatWindow</div>;
+  const state = store.getState();
+  const activeUser = state.contacts[activeUserId];
+  console.log(state);
+  return (
+    <div className="ChatWindow">
+      <Header user={activeUser} />
+      <Chats />
+      <MessageInput />
+    </div>
+  );
 }
 
 export default ChatWindow;
